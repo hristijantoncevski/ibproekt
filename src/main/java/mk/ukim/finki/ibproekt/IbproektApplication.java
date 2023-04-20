529125a98e832225fc7ad2cb2924fbdcc8e4f59c
hristijantoncevski <hristijan.tonchevski@students.finki.ukim.mk>
2023-04-20 14:27:51 +0200

    init

---
+++ src/main/java/mk/ukim/finki/ibproekt/IbproektApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ibproekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbproektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbproektApplication.class, args);
+    }
+
+}
